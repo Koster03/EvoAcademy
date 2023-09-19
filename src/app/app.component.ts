7c86f39ce0688310e493543f0abb4afd68763de0
Koster03 <pyatkin-kostik@mail.ru>
2023-09-19 10:48:22 +0300

    Directives&Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'EvoAcademy';
+  hello = 'Hello';
+
+  public cars = [
+    {
+      Name: 'Audi',
+      Price: 100,
+      Release: new Date(),
+    },
+    {
+      Name: 'BMW',
+      Price: 200,
+      Release: new Date(),
+    },
+    {
+      Name: 'HAVAL',
+      Price: 300,
+      Release: new Date(),
+    },
+    {
+      Name: 'LADA',
+      Price: 400,
+      Release: new Date(),
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlusFivePipe } from './pipes/plus-five.pipe';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlusFivePipe,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
