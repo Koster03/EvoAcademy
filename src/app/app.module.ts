66e7c2f4aa4c759b1222d338ed75bd34a1fde5f0
Koster03 <pyatkin-kostik@mail.ru>
2023-10-03 23:01:29 +0300

    Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatDividerModule, 
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private http = inject(HttpClient);
+
+  public get() {
+    return this.http
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((data) => console.log(data));
+  }
+
+  public getWithParams() {
+    return this.http
+      .get('https://jsonplaceholder.typicode.com/comments', {
+        params: { postId: 1 },
+      })
+      .subscribe((data) => console.log(data));
+  }
+
+  public post() {
+    return this.http
+      .post(' https://jsonplaceholder.typicode.com/posts', {})
+      .subscribe((data) => console.log(data));
+  }
+
+  public getError() {
+    return this.http
+      .get('https://jsonplaceholder.typicode.com/post', {
+        params: { postId: 1 },
+      })
+      .subscribe({
+        next: (data) => console.log(data),
+        error: (err: HttpErrorResponse) => {
+          console.log(err.message);
+        },
+      });
+  }
+
+  public getWithHeader() {
+    return this.http
+      .get('https://jsonplaceholder.typicode.com/posts', {
+        headers: { 'X-Test': '1' },
+      })
+      .pipe(
+        map(data => JSON.stringify(data)),
+      )
+      .subscribe((data) => console.log(data));
+  }
+
+  public delete() {
+    return this.http
+      .delete('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe((data) => console.log(data));
+  }
+}
